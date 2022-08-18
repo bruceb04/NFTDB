e7d74575bf3dfaa8148e84344e3ce07b5be9b854
Bruce Bermel <brucebermel14@gmail.com>
2022-08-17 20:06:00 -0400

    GHETSIS AND EISCUE

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
 // @match      https://*.psim.us/
 // @match      http://*.psim.us/*
 // @match      https://*.psim.us/*
-// @version     1.0.6
+// @version     1.0.7
 // @grant       none
 // ==/UserScript==
 
@@ -24,11 +24,11 @@ const queries = ["https://play.pokemonshowdown.com/sprites/trainers/erika-gen2.p
                    "https://play.pokemonshowdown.com/sprites/trainers/expertf-gen6.png", "https://play.pokemonshowdown.com/sprites/trainers/beauty-gen6xy.png", 
                    "https://play.pokemonshowdown.com/sprites/trainers/sidney-gen3.png","https://play.pokemonshowdown.com/sprites/trainers/lusamine-masters.png",
                 "https://play.pokemonshowdown.com/sprites/trainers/archie-gen6.png", "https://play.pokemonshowdown.com/sprites/trainers/n-masters.png", 
-                "https://play.pokemonshowdown.com/sprites/trainers/youngster-gen1rb.png"];
+                "https://play.pokemonshowdown.com/sprites/trainers/youngster-gen1rb.png", "https://play.pokemonshowdown.com/sprites/trainers/fisherman-gen2jp.png"];
 const replacements = ["https://i.imgur.com/0TN5NDe.png", "https://i.imgur.com/TwJ1x9G.png", "https://i.imgur.com/ObCckQn.png",
                         "https://i.imgur.com/onhKdqk.png", "https://i.imgur.com/dUYdPN0.png", "https://i.imgur.com/DxuupCk.png",
                        "https://i.imgur.com/AbWrOcY.png", "https://i.imgur.com/j0Gu8WW.png", "https://i.imgur.com/Iphkl7n.png",
-                        "https://i.imgur.com/5sVe5S3.png", "https://i.imgur.com/KrfdiLH.png"];
+                        "https://i.imgur.com/5sVe5S3.png", "https://i.imgur.com/KrfdiLH.png", "https://i.imgur.com/gSl3nrz.png"];
 
 
 setInterval(function() {
