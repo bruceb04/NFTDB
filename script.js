0c7cf1a73600605a7909670401557dc49d24f3ea
Bruce Bermel <brucebermel14@gmail.com>
2023-03-22 11:34:49 -0400

    updated version

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
 // @match      https://*.psim.us/
 // @match      http://*.psim.us/*
 // @match      https://*.psim.us/*
-// @version     2.0.6
+// @version     2.0.7
 // @grant       none
 // ==/UserScript==
 
